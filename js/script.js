754c4ccd8ded2b5d3f67bea7cd334544702902ef
Dayana Pulido <144375079+Bygyacqq@users.noreply.github.com>
2024-09-27 23:10:49 -0500

    parte testimonios

--- js/script.js
+++ js/script.js
@@ -10,3 +10,20 @@ document.getElementById('menu_toggle').addEventListener('click', function() {
     const menu = document.getElementById('menu');
     menu.classList.toggle('active');
 })
+const videoContainer = document.querySelector('.video-container');
+const leftButton = document.querySelector('.slider-left');
+const rightButton = document.querySelector('.slider-right');
+const scrollAmount = 320;
+leftButton.addEventListener('click', () => {
+    videoContainer.scrollBy({
+        left: -scrollAmount,
+        behavior: 'smooth'
+    });
+});
+
+rightButton.addEventListener('click', () => {
+    videoContainer.scrollBy({
+        left: scrollAmount,
+        behavior: 'smooth'
+    });
+});
